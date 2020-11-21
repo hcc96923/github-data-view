f4308634d0e24aa4ab8da1fd3909176359b58ddc
韩畅畅 <hcc96923@gmail.com>
2020-11-22 00:10:58 +0800

    优化top

--- src/views/Data/DataCenter/Card/index.js
+++ src/views/Data/DataCenter/Card/index.js
@@ -10,18 +10,36 @@ function Card() {
                     <BankOutlined />
                 </section>
                 <section className="card_item_right">
-                    <span className="card_item_title">公开仓库数</span>
+                    <span className="card_item_title">公开仓库</span>
                     <span className="card_item_count">7</span>
                 </section>
             </div>
             <div className="card_item">
-
+                <section className="card_item_left">
+                    <BankOutlined />
+                </section>
+                <section className="card_item_right">
+                    <span className="card_item_title">私有仓库</span>
+                    <span className="card_item_count">1</span>
+                </section>
             </div>
             <div className="card_item">
-
+                <section className="card_item_left">
+                    <BankOutlined />
+                </section>
+                <section className="card_item_right">
+                    <span className="card_item_title">粉丝数量</span>
+                    <span className="card_item_count">1</span>
+                </section>
             </div>
             <div className="card_item">
-
+                <section className="card_item_left">
+                    <BankOutlined />
+                </section>
+                <section className="card_item_right">
+                    <span className="card_item_title">跟随数量</span>
+                    <span className="card_item_count">12</span>
+                </section>
             </div>
         </div>
     )
